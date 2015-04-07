2e1ea048b442d3b7ebbfefa0b89a31f3b163cd66
mdeboer1 <mdeboer1@my.wctc.edu>
2015-04-07 14:25:32 -0500

    Began working with Spring to redirect to different pages based on admin vs user.  Needs new controller for user still to work.  Current controller is redirecting back to hotelmanagement.jsp and not hotellookup.jsp.

---
+++ src/main/java/com/mycompany/mavenhotelwebjpa/util/MyAuthenticationHandler.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenhotelwebjpa.util;
+
+import java.io.IOException;
+import java.util.Set;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+/**
+ *
+ * @author jlombardo
+ */
+public class MyAuthenticationHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+        String userTargetUrl = "/hotellookup.jsp"; // change "user" to whatever you use
+        String adminTargetUrl = "/hotelmanagement.jsp";  // change "admin" to whatever you use and add more taretURLs if needed
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        if (roles.contains("ROLE_ADMIN")) {
+            getRedirectStrategy().sendRedirect(request, response, adminTargetUrl);
+        } else if (roles.contains("ROLE_USER")) {
+            getRedirectStrategy().sendRedirect(request, response, userTargetUrl);
+        } else {
+            super.onAuthenticationSuccess(request, response, authentication);
+            return;
+        }
+    }
+}
